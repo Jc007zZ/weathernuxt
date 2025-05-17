706bf4bfd3b1bb7a8c004cfb47caec819d12354f
Jc007zZ <joaocpmcs@gmail.com>
2025-05-17 20:32:31 -0300

    geolocale remove

--- components/search.vue
+++ components/search.vue
@@ -53,32 +53,6 @@ async function handleOptionSelected(option: SearchResult) {
   searchValue.value = '';
 }
 
-// makes a request based on your location
-function geoLocateUser() {
-  async function success(position: GeolocationPosition) {
-    lagLong.lat = position.coords.latitude
-    lagLong.long = position.coords.longitude
-
-    // Notify parent for title change
-    emit('locationChanged', 'Your Location')
-
-    // Reset state
-    searchOptionsOpen.value = false;
-    searchResults.value = [];
-    searchValue.value = '';
-  }
-
-  function error() {
-    alert('Could not locate user');
-  }
-
-  if (!navigator.geolocation) {
-    alert('Geolocation not supported by your browser');
-  } else {
-    emit('startTransition')
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-}
 </script>
 <template>
   <div class="w-full flex gap-4 lg:gap-8 items-center">
@@ -101,13 +75,6 @@ function geoLocateUser() {
       </ul>
     </div>
 
-    <!-- Get current location  -->
-    <button class="w-16 h-16 rounded bg-slate-200 hover:bg-slate-300 text-slate-700" @click="geoLocateUser"
-      title="Use your current location">
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 m-auto" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-        stroke-Width="2" stroke-Linecap="round" stroke-Linejoin="round">
-        <polygon points="3 11 22 2 13 21 11 13 3 11" />
-      </svg>
-    </button>
+
   </div>
 </template>
